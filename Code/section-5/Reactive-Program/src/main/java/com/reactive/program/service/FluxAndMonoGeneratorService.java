044b9f3b37bb7e39d8ed065eadec2ae8ef7b64fb
polas-cse <mdpolashossein@gmail.com>
2025-10-20 18:26:43 +0600

    mono

--- Code/section-5/Reactive-Program/src/main/java/com/reactive/program/service/FluxAndMonoGeneratorService.java
+++ Code/section-5/Reactive-Program/src/main/java/com/reactive/program/service/FluxAndMonoGeneratorService.java
@@ -1,30 +1,37 @@
 package com.reactive.program.service;
 
 import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
 
 import java.util.List;
 
 public class FluxAndMonoGeneratorService {
 
-    private Flux<String> getNames(){
+    private Flux<String> getFluxNames(){
         Flux<String> names = Flux.fromIterable(List.of("Polas","Rasel", "Naime", "Jakariya"));
+        return names;
+    }
+
+    private Mono<String> getMonoNames(){
+        Mono<String> names = Mono.just("Polas");
         return names;
     }
 
     public static void main(String[] args) {
 
         FluxAndMonoGeneratorService obj = new FluxAndMonoGeneratorService();
-        Flux<String> names = obj.getNames();
+        Flux<String> fluxNames = obj.getFluxNames();
 
-        System.out.println("Subscribe");
-        names.filter(name-> name.length() <= 5)
-                .subscribe(System.out::println);
-
-        System.out.println("\nSubscribe");
-        names.subscribe(name->{
+        System.out.println("Flux");
+        fluxNames.subscribe(name->{
             System.out.println("Name = " + name);
         });
 
+        System.out.println("\nMono");
+        Mono<String> monoNames = obj.getMonoNames();
+        monoNames.subscribe(name->{
+            System.out.println("Name = " + name);
+        });
     }
 
 }
